89d4aee40650bed84af1ccb50fe4a347d4f5ad81
Patrick Muller <patrick.e.muller@gmail.com>
2025-09-14 14:54:55 -0700

    Update Devise configs & remove unused code
    - Change `mailer_sender` to `no-reply@find-my.club`.
    - Increase minimum password length to 8 characters.
    - Reduce password reset window to 2 hours.
    - Remove unused `Users::RegistrationsHelper`.
    - Remove `Users::RegistrationsControllerTest`.
    - Comment out unused `PagesControllerTest`.

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get root_path
-    assert_response :success
-  end
+  # test "should get home" do
+  #   get root_path
+  #   assert_response :success
+  # end
 end
