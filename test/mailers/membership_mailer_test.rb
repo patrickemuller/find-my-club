a6c8156fe5c0b213b1f5f7b2b92ed5a86bbcf6f1
Patrick Muller <patrick.e.muller@gmail.com>
2025-10-21 18:03:49 -0700

    Fix tests for membership feature

--- test/mailers/membership_mailer_test.rb
+++ test/mailers/membership_mailer_test.rb
@@ -2,9 +2,10 @@ require "test_helper"
 
 class MembershipMailerTest < ActionMailer::TestCase
   test "approved" do
-    mail = MembershipMailer.approved
-    assert_equal "Approved", mail.subject
-    assert_equal [ "to@example.org" ], mail.to
+    membership = FactoryBot.create(:membership)
+    mail = MembershipMailer.approved(membership)
+    assert_equal "You've been approved to join #{membership.club.name}!", mail.subject
+    assert_equal [ membership.user.email ], mail.to
     assert_equal [ "from@example.com" ], mail.from
     assert_match "Hi", mail.body.encoded
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,10 +2,13 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
+# Ensure Devise mappings are loaded
+Rails.application.reload_routes!
+
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
+    # parallelize(workers: :number_of_processors)
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
@@ -15,4 +18,9 @@ module ActiveSupport
 
     # Add more helper methods to be used by all tests here...
   end
+end
+
+# Include Devise test helpers for integration tests
+class ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
 end
