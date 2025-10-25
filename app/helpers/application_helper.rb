807a3c94358a7471c18f7940eded93f438518205
Patrick Muller <patrick.e.muller@gmail.com>
2025-10-24 23:24:35 -0700

    Change text to "join the waitlist" for waitlist events
    - The text appears when the event is full and there is a waitlist option
    - Added an inline_svg_tag helper specific to display SVG icons next to text
    - Added Google Calendar SVG icon next to the "Gogole Calendar" link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def inline_svg(filename, options = {})
+    file_path = Rails.root.join("app", "assets", "icons", "#{filename}.svg")
+    return unless File.exist?(file_path)
+
+    svg = File.read(file_path)
+
+    # Add CSS classes if provided
+    if options[:class].present?
+      svg = svg.sub(/<svg/, "<svg class=\"#{options[:class]}\"")
+    end
+
+    svg.html_safe
+  end
 end
