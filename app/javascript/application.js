d9e739c10ec0ce9539e16aa7e4aae7dc3f5e3dcb
Patrick Muller <patrick.e.muller@gmail.com>
2025-11-11 19:25:46 -0800

    Add CloudFlare R2 as file storage mechanism in production
    - This will allow users to have profile pictures
    - Club's descriptions can also have images if they want

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,9 +4,3 @@ import "controllers"
 
 import "trix"
 import "@rails/actiontext"
-
-// Disable file attachments in Trix editor
-document.addEventListener("trix-file-accept", function(event) {
-  event.preventDefault()
-  alert("File uploads are not supported at this time")
-})
