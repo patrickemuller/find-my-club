0f4434ae5b95c479d175a4d37acaf9731a67879d
Patrick Muller <patrick.e.muller@gmail.com>
2025-11-28 19:10:07 -0800

    Add Ngrok to allowed hosts

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module FindMyClub
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.mission_control.jobs.http_basic_auth_enabled = false
+
+    config.hosts << ".ngrok-free.app"
   end
 end
